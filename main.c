41082a43c151d1139587109366ff9d1944ce4f11
ahmetlekesiz <ahmetlekesiz@gmail.com>
2019-11-26 14:48:11 +0300

    scanf

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    //initilize input sequence
+    int size = 200;
+    int input[size];
+    for (int i = 0; i < size; ++i) {
+        input[i] = -1;
+    }
+
+    //take input from the user and store in the array
+    printf("Please enter the sequence to build the Binary Search Tree. \n");
+
+    //build bst topology
+    //find how many different sequences yield the same BST Topology.
     return 0;
 }
